96c918b019fa481306aeb13d766f85e6aa21003f
Victor Villas <villasv@outlook.com>
2017-02-13 12:27:22 -0200

    init python package

---
+++ FingerprintPy/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fingerpint',
+    version='0.1.0',
+    platforms='any',
+    description='Identity recognition by keystroke dynamics',
+    author='Victor Villas',
+    author_email='villasv@outlook.com',
+    url='https://github.com/LucasIME/Fingerprint',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.5'
+    ]
+)
