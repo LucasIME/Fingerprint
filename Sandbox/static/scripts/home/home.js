f0c0254261978fadf50b0fc17d065bffdd74c724
Lucas Meireles <meireles31@msn.com>
2017-02-13 11:59:33 -0200

    Created sandbox environment.

---
+++ Sandbox/static/scripts/home/home.js
@@ -0,0 +1,22 @@
+angular.module('app.home', ['ngRoute'])
+
+.config(function($routeProvider) {
+   $routeProvider
+   .when('/', {
+      templateUrl: 'static/scripts/home/home.html',
+      controller : 'HomeCtrl',
+   });
+})
+
+.controller('HomeCtrl', function($scope, $http){
+    var self = this;
+    $scope.welcome_message = "Fingerprint sandbox";
+    $scope.training_paragraph = "This is our training paragraph. It has UPPERCASE and lowercase \
+                                characters to better learn your typing pattern. It also has all the \
+                                letters in the alphabet like in the sentence: The quick brown fox jumps \
+                                over the lazy dog";
+
+    $scope.send_typing_data = function(){
+        //TODO
+    };
+});
