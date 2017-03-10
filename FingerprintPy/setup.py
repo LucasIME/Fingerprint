5545fbd4d7e9c4f61c400c91879da391bddb6943
Lucas Meireles <meireles31@msn.com>
2017-03-10 07:41:28 -0300

    fixed typo on setup.py

--- FingerprintPy/setup.py
+++ FingerprintPy/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='fingerpint',
+    name='fingerprint',
     version='0.1.0',
     platforms='any',
     description='Identity recognition by keystroke dynamics',
