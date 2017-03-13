b87d9814b2662c3e4e2272e103eae3e09944aa15
Lucas Meireles <meireles31@msn.com>
2017-03-13 11:30:46 -0300

    Created endpoints to check, delete, create and authenticate users.

--- Sandbox/server.py
+++ Sandbox/server.py
@@ -5,6 +5,7 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -14,6 +15,34 @@ def fingerprint():
     keystroke_stream = json.loads(request.data.decode())
     return jsonify({
         "fingerprint": identify(keystroke_stream)
+    })
+
+@app.route('/user/<user_id>', methods=['GET'])
+def checkUser(user_id):
+    #TODO: implement
+    return jsonify({
+        "message": "User exists!"
+    })
+
+@app.route('/user/<user_id>', methods=['DELETE'])
+def deleteUser(user_id):
+    #TODO: implement
+    return jsonify({
+        "message": "User deleted successfully!"
+    })
+
+@app.route('/save/<user_id>', methods=['POST'])
+def savePattern(user_id):
+    #TODO: implement
+    return jsonify({
+        "message": "Created pattern for user " + user_id
+    })
+
+@app.route('/verify/<user_id>', methods=['POST'])
+def verifyPattern(user_id):
+    #TODO: implement
+    return jsonify({
+        "message": "User verified successfully!"
     })
 
 if __name__ == '__main__':
