3dd8cfce72b02bb1ce80d054847b4e417fb3eaf7
Lucas Meireles <meireles31@msn.com>
2017-07-02 17:19:55 +0000

    Webpack has a single entry point now

--- Sandbox/static/scripts/webpack.config.js
+++ Sandbox/static/scripts/webpack.config.js
@@ -2,14 +2,6 @@ var path = require('path')
 
 module.exports = {
     entry: './main.js',
-    //{
-    //     main: './main.js',
-    //     register: './register/register.js',
-    //     auth: './auth/auth.js',
-    //     home: './home/home.js',
-    //     navbar: './navbar/navbar.js',
-    //     fingerprint: './fingerprint.js',
-    // },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
