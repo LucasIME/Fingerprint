50756dbeb9b28fba7fe4f98d2705fd652929b5cc
Lucas Meireles <meireles31@msn.com>
2017-07-11 16:53:34 -0300

    Added source map

--- Sandbox/static/scripts/webpack.config.js
+++ Sandbox/static/scripts/webpack.config.js
@@ -5,5 +5,23 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+
+    module: {
+        rules: [
+            {
+                // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+                enforce: 'pre',
+                test: /\.js$/, 
+                loader: "source-map-loader"
+            }
+        ]
     }
 };
