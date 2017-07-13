7c06b15abaec8959e25124f652fb59eff8652601
Lucas Meireles <meireles31@msn.com>
2017-07-13 17:01:43 -0300

    noew ring buffer lib is written in typescript

--- FingerprintJS/src/index.ts
+++ FingerprintJS/src/index.ts
@@ -1,26 +1,32 @@
 "use strict";
 
-class RingBuffer{
-    constructor(size){
+class RingBuffer<T>{
+    
+    buffer : Array<T>;
+    len : number;
+    currentIndex : number;
+    totalAdded : number;
+
+    constructor(size: number){
         this.buffer = new Array(size);
         this.len = size;
         this.currentIndex = 0;
         this.totalAdded = 0;
     }
 
-    push(obj){
+    push(obj : T) : void{
         this.buffer[this.currentIndex++] = obj;
         this.currentIndex %= this.len;
         this.totalAdded++;
     }
 
-    getLast(n){
+    getLast(n : number) : Array<T>{
         if (n > this.len)
             throw new Error('Number should be smaller than Ring Buffer length');
         if(n > this.totalAdded)
             throw new Error("Number should not be greater than the number of items added to the Ring Buffer");
-        var first = this.currentIndex;
-        var last = 0;
+        let first = this.currentIndex;
+        let last = 0;
         if(first >= 1){
             last = first - 1;
             if(last+1 >= n)
@@ -35,23 +41,26 @@ class RingBuffer{
     }
 }
 
-class Fingerprint{
-    constructor(bufferSize){
-        this.buffer = new RingBuffer(bufferSize);
+export class Fingerprint{
+    buffer : RingBuffer<any>;
+
+    constructor(bufferSize : number){
+        this.buffer = new RingBuffer<any>(bufferSize);
     }
 
-    processKeyDownEvent(evt){
+    processKeyDownEvent(evt) : void{
         this.buffer.push([evt.key, "DOWN", (new Date()).getTime()]);
         console.log("DOWN", evt);
     }
 
-    processKeyUpEvent(evt){
+    processKeyUpEvent(evt) : void{
         this.buffer.push([evt.key, "UP", (new Date()).getTime()]);
         console.log("UP", evt);
     }
 
-    get(length){
-        var n = length || Math.min(this.buffer.len, this.buffer.totalAdded);
+    get(length : number) : Array<any> {
+        let n = length || Math.min(this.buffer.len, this.buffer.totalAdded);
         return this.buffer.getLast(n);
     }
 }
+
