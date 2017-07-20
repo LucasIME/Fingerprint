0478f841b08b7c825917b5b5fc65710901756322
Lucas Meireles <meireles31@msn.com>
2017-07-20 10:26:07 -0300

    Starting to extract features from keystroke stream. Implemented down-up average time.

--- FingerprintPy/fingerprint/__init__.py
+++ FingerprintPy/fingerprint/__init__.py
@@ -1,3 +1,24 @@
 
 def identify(keystroke_stream):
-    return None
+    features = extract_features(keystroke_stream)
+    return features
+
+def extract_features(keystroke_stream):
+    down_up_average = get_down_up_average(keystroke_stream)
+    return {
+        "down_up_average" : down_up_average
+    }
+
+def get_down_up_average(keystroke_stream):
+    stack = []
+    total_time = 0
+    number_of_down_up_pairs = 0
+    for keystroke in keystroke_stream:
+        if keystroke[1] == 'DOWN':
+            stack.append(keystroke)
+        elif keystroke[1] == 'UP':
+            if stack:
+                top_keystroke = stack.pop()
+                total_time += keystroke[2] - top_keystroke[2]
+                number_of_down_up_pairs += 1
+    return total_time/number_of_down_up_pairs
