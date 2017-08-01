2fa45b17a47cbd0b37bb52437411b79fb9604443
Lucas Meireles <meireles31@msn.com>
2017-08-01 17:39:45 -0300

    now saving features on mongodb when registering user

--- Sandbox/server.py
+++ Sandbox/server.py
@@ -1,9 +1,15 @@
 from flask import Flask, request, render_template, jsonify
 from fingerprint import identify
 import json
+import os
+import pymongo
 
 
 app = Flask(__name__)
+
+db_connection_url = os.environ['DATABASE_URI']
+db_client = pymongo.MongoClient(db_connection_url)
+fingerprint_db = db_client.fingerprint
 
 
 @app.route('/')
@@ -34,8 +40,13 @@ def deleteUser(user_id):
 @app.route('/save/<user_id>', methods=['POST'])
 def savePattern(user_id):
     #TODO: implement
+    keystroke_stream = json.loads(request.data.decode())
+    features = identify(keystroke_stream)
+    fingerprint_db.features.insert_one({'email':user_id, 'features':features})
+
     return jsonify({
-        "message": "Created pattern for user " + user_id
+        "message": "Created pattern for user " + user_id,
+        "fingerprint": features
     })
 
 @app.route('/auth/<user_id>', methods=['POST'])
