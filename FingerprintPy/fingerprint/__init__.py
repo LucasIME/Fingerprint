8cbe069eb05fc1bd90609adba80b8da2b1b0533f
Lucas Meireles <meireles31@msn.com>
2017-08-01 14:44:13 -0300

    Added down_down average feature

--- FingerprintPy/fingerprint/__init__.py
+++ FingerprintPy/fingerprint/__init__.py
@@ -10,9 +10,11 @@ def identify(keystroke_stream):
 def extract_features(keystroke_stream):
     down_up_average = get_down_up_average(keystroke_stream)
     up_down_average = get_up_down_average(keystroke_stream)
+    down_down_average = get_down_down_average(keystroke_stream)
     return {
         "down_up_average" : down_up_average,
-        "up_down_average" : up_down_average
+        "up_down_average" : up_down_average,
+        "down_down_average" : down_down_average,
     }
 
 def get_down_up_average(keystroke_stream):
@@ -42,4 +44,18 @@ def get_up_down_average(keystroke_stream):
                 total_time += keystroke['timestamp'] - top_keystroke['timestamp']
                 number_of_up_down_pairs += 1
     return total_time/number_of_up_down_pairs if number_of_up_down_pairs != 0 else None
+
+def get_down_down_average(keystroke_stream):
+    last_timestamp = None
+    total_time = 0
+    number_of_down_down_pairs = 0
+    for keystroke in keystroke_stream:
+        if keystroke['direction'] == 'DOWN':
+            if last_timestamp is None:
+                last_timestamp = keystroke['timestamp']
+            else:
+                total_time += keystroke['timestamp'] - last_timestamp
+                number_of_down_down_pairs += 1
+                last_timestamp = keystroke['timestamp']
+    return total_time/number_of_down_down_pairs if number_of_down_down_pairs != 0 else None
 
