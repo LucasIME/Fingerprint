cafd7794eb856b2e4e42cf5ee339963782232169
Lucas Meireles <meireles31@msn.com>
2017-08-15 09:55:53 -0300

    Created new functions for getting key hold time and DD time between keys.

--- FingerprintPy/fingerprint/__init__.py
+++ FingerprintPy/fingerprint/__init__.py
@@ -3,21 +3,24 @@ class Keystroke:
     def __init__(self):
         pass
 
+def char_range(c1, c2):
+    """Generates the characters from `c1` to `c2`, inclusive."""
+    for c in range(ord(c1), ord(c2)+1):
+        yield chr(c)
+
 def identify(keystroke_stream):
     features = extract_features(keystroke_stream)
     return features
 
 def extract_features(keystroke_stream):
-    down_up_average = get_down_up_average(keystroke_stream)
-    up_down_average = get_up_down_average(keystroke_stream)
-    down_down_average = get_down_down_average(keystroke_stream)
-    up_up_average = get_up_up_average(keystroke_stream)
-    return {
-        "down_up_average" : down_up_average,
-        "up_down_average" : up_down_average,
-        "down_down_average" : down_down_average,
-        "up_up_average" : up_up_average
-    }
+    resp = {}
+
+    for c in char_range('a', 'z'):
+        key = c + '_hold_avg'
+        value = get_hold_avg_for_key(keystroke_stream, c)
+        resp[key] = value
+    
+    return resp
 
 def get_down_up_average(keystroke_stream):
     stack = []
@@ -74,3 +77,35 @@ def get_up_up_average(keystroke_stream):
                 number_of_up_up_pairs += 1
                 last_timestamp = keystroke['timestamp']
     return total_time/number_of_up_up_pairs if number_of_up_up_pairs != 0 else None
+
+def get_hold_avg_for_key(keystroke_stream, key):
+    last_timestamp = None
+    total_time = 0
+    number_of_matches = 0
+    for keystroke in keystroke_stream:
+        if keystroke['key'] == key:    
+            if keystroke['direction'] == 'DOWN':
+                last_timestamp = keystroke['timestamp']
+            elif keystroke['direction'] == 'UP':
+                if last_timestamp is not None:
+                    total_time += keystroke['timestamp'] - last_timestamp
+                    number_of_matches += 1
+                    last_timestamp = keystroke['timestamp']
+    return total_time/number_of_matches if number_of_matches != 0 else None
+
+def get_down_down_avg_for_digraph(keystroke_stream, key1, key2):
+    last_timestamp = None
+    total_time = 0
+    number_of_matches = 0
+    i = 0
+    while i < len(keystroke_stream):
+        cur_keystroke = keystroke_stream[i]
+        if cur_keystroke['key'] == key1 and cur_keystroke['direction'] == 'DOWN':
+            next_down_i = get_next_down(keystroke_stream, i)
+            if keystroke_stream[next_down_i]['key'] == key2:
+                total_time += keystroke_stream[next_down_i]['timestamp'] - cur_keystroke['timestamp']
+                number_of_matches += 1
+            i = next_down_i
+        else:
+            i+=1
+    return total_time/number_of_matches if number_of_matches != 0 else None
