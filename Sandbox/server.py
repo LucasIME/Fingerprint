840f82f58d50ec9f43124920504e42518767504f
Lucas Meireles <meireles31@msn.com>
2017-09-27 14:31:24 -0300

    Now adding possibility to use unique model for all users

--- Sandbox/server.py
+++ Sandbox/server.py
@@ -18,6 +18,18 @@ db_connection_url = os.environ['DATABASE_URI']
 db_client = pymongo.MongoClient(db_connection_url)
 fingerprint_db = db_client.fingerprint
 
+def to_class(email):
+    d = {
+        'demeterkoime@gmail.com' : 0,
+        'narcelio@outlook.com' : 1,
+        'villasv@outlook.com' : 2,
+        'meireles31@msn.com' : 3,
+        'psmeireles25@gmail.com' : 4, 
+        'brunomatissek@hotmail.com' : 5, 
+        'pedroplpa@hotmail.com' : 6, 
+        'almeida.caio1@gmail.com' : 7,
+    }
+    return d[email] if email in d else -1
 
 @app.route('/')
 def home():
@@ -33,6 +45,8 @@ def delete_user(user_id):
 @app.route('/user/<user_id>', methods=['POST'])
 @swag_from('/docs/savepattern.yml')
 def save_pattern(user_id):
+    fingerprint_db.email_id.update_one({'email':user_id}, {'$setOnInsert':{'email':user_id}, '$set':{'id':to_class(user_id)}}, upsert=True)
+
     keystroke_stream = json.loads(request.data.decode())
     features = identify(keystroke_stream)
     fingerprint_db.features.insert_one({'email':user_id, 'features':features})
@@ -41,6 +55,9 @@ def save_pattern(user_id):
     recalculate_features()
     user_model = create_model_for_id(user_id)
     save_model(user_model, user_id)
+
+    unique_model = create_unique_model()
+    save_unique_model(unique_model)
 
     return jsonify({
         "wasUserCreated" : True,
@@ -58,10 +75,11 @@ def verify_pattern(user_id):
 
     all_users = get_all_users_features_dataframe()
 
-    clf = load_model_from_user(user_id)
+    #clf = load_model_from_user(user_id)
+    clf = load_unique_model()
 
     target_array =  target_array.fillna(all_users.mean())
-    print(target_array)
+
     response = clf.predict(target_array)
     print('Response for user {0}: {1}'.format(user_id, response))
     return jsonify({
@@ -88,12 +106,29 @@ def create_model_for_id(user_id):
     result = pd.concat([user_features, non_user_features]).reset_index(drop=True)
     X = result.fillna(result.mean())
     Y = [1 for i in range(len(user_features))] + [0 for i in range(len(non_user_features.index))]
-    
+
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(X, Y)
 
     print(metrics.confusion_matrix(clf.predict(X), Y))
     print(clf.feature_importances_)
+    tree.export_graphviz(clf, out_file=(str(user_id)+'.dot'))
+
+    return clf
+
+def create_unique_model():
+    raw_user_features = list(fingerprint_db.features.find({}))
+    user_features = pd.DataFrame([user['features'] for user in raw_user_features]).reset_index(drop=True)
+
+    X = user_features.fillna(user_features.mean())
+    Y = [to_class(user['email']) for user in raw_user_features]
+
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(X, Y)
+
+    print('Unique Model Confusion Matrix: ', metrics.confusion_matrix(clf.predict(X), Y))
+    print('Unique model feature importance: ', clf.feature_importances_)
+    tree.export_graphviz(clf, out_file='uniquemodel.dot')
 
     return clf
 
@@ -106,8 +141,18 @@ def save_model(model_obj, user_id):
     binary_model = pickle.dumps(model_obj)
     fingerprint_db.models.update_one({'email': user_id}, {'$setOnInsert':{'email': user_id}, '$set':{'model': binary_model}}, upsert=True)
 
+def save_unique_model(model_obj):
+    fingerprint_db.unique_model.remove()
+    binary_model = pickle.dumps(model_obj)
+    fingerprint_db.unique_model.insert_one({'model': binary_model})
+
 def load_model_from_user(user_id):
     binary_model = fingerprint_db.models.find_one({'email': user_id})
+    model = pickle.loads(binary_model['model'])
+    return model
+
+def load_unique_model():
+    binary_model = fingerprint_db.unique_model.find_one({})
     model = pickle.loads(binary_model['model'])
     return model
 
@@ -116,6 +161,9 @@ def create_all_models():
         model = create_model_for_id(document['email'])
         save_model(model, document['email'])
     print("Created new models for all users!")
+    unique_model = create_unique_model()
+    save_unique_model(unique_model)
+    print('Create unique model!')
 
 if __name__ == '__main__':
     recalculate_features()
